f946b4fa39141823437001b0854b5ae0b804cd46
joaoassisdelimafilho <joaoassis.scp@gmail.com>
2026-03-20 02:52:45 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    nome_loja: "ISABELA",
+    logo_url: "logo.jpg",
+    assinatura_rodape: "EST. 2026 | ALTA CURADORIA",
+    whatsapp: "https://wa.me/5511999999999", 
+    catalogo_whatsapp: "https://wa.me/c/5519995027183",
+    facebook_page: "https://facebook.com/suapagina",
+    rota_localizacao: "https://maps.app.goo.gl/gKyVErJkxJS2zWLR7?g_st=ac",
+    instagram: "https://instagram.com/seuuser",
+    tiktok: "https://tiktok.com/@seuuser"
+};
